681ee6905ca7474768c213cfe43f3d6fb80b48b0
jiayu-ch15 <jiayu-ch15@mails.tsinghua.edu.cn>
2021-12-07 17:19:45 +0800

    update

--- scripts/train_hns.sh
+++ scripts/train_hns.sh
@@ -7,8 +7,8 @@ num_boxes=1
 num_ramps=1
 floor_size=6.0
 task_type='all-return'
-algo='horizon40_step3'
+algo='horizon60_step3'
 seed=1
 
-CUDA_VISIBLE_DEVICES=3 python train_hns.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario_name} --num_hiders ${num_hiders} --num_seekers ${num_seekers} --num_boxes ${num_boxes} --num_ramps ${num_ramps}  --task_type ${task_type} --seed ${seed} --floor_size ${floor_size}  --n_rollout_threads 300 --num_mini_batch 2 --episode_length 40 --env_horizon 40 --num_env_steps 400000000 --ppo_epoch 15 --attn --save_interval 1 --eval \
+CUDA_VISIBLE_DEVICES=1 python train_hns.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario_name} --num_hiders ${num_hiders} --num_seekers ${num_seekers} --num_boxes ${num_boxes} --num_ramps ${num_ramps}  --task_type ${task_type} --seed ${seed} --floor_size ${floor_size}  --n_rollout_threads 300 --num_mini_batch 2 --episode_length 60 --env_horizon 60 --num_env_steps 400000000 --ppo_epoch 15 --attn --save_interval 1 --eval \
 --buffer_length 2000 --epsilon 3 --delta 3 --sol_prop 0.05 --B_exp 200 --h 1 --archive_initial_length 500 --eval_number 3 --use_wandb
