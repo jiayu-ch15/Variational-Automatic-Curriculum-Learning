673f520eb89ca69158418a894f79d02733e00533
jiayu-ch15 <jiayu-ch15@jiayu.eva5.nics.info>
2023-02-07 09:20:39 +0000

    debug

--- scripts/train_mpe_woEP.sh
+++ scripts/train_mpe_woEP.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 ulimit -n 32768
 env="MPE"
-scenario="simple_spread"
-num_landmarks=4
-num_agents=4
+scenario="push_ball"
+num_landmarks=2
+num_agents=2
 algo="debug"
 seed=1
 
@@ -11,8 +11,8 @@ echo "env is ${env}, scenario is ${scenario}, algo is ${algo}, seed is ${seed}"
 
 
 # simple spread
-CUDA_VISIBLE_DEVICES=1 python train_mpe_woEP.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_rollout_threads 2 --num_mini_batch 2 --episode_length 70 --num_env_steps 60000000 --ppo_epoch 15 --recurrent_policy --use_popart --lr 5e-4 --use_accumulate_grad \
---buffer_length 2000 --epsilon 0.6 --delta 0.6 --sol_prop 0.05 --archive_initial_length 500 --B_exp 150
+# CUDA_VISIBLE_DEVICES=1 python train_mpe_woEP.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_rollout_threads 2 --num_mini_batch 2 --episode_length 70 --num_env_steps 60000000 --ppo_epoch 15 --recurrent_policy --use_popart --lr 5e-4 --use_accumulate_grad \
+# --buffer_length 2000 --epsilon 0.6 --delta 0.6 --sol_prop 0.05 --archive_initial_length 500 --B_exp 150
 
 
 # hard spread
@@ -20,7 +20,8 @@ CUDA_VISIBLE_DEVICES=1 python train_mpe_woEP.py --env_name ${env} --algorithm_na
 # --buffer_length 2000 --epsilon 0.6 --delta 0.6 --h 1000 --archive_initial_length 500 --B_exp 300 --use_wandb
 
 # push ball
-# CUDA_VISIBLE_DEVICES=0 python train_mpe_woEP.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_rollout_threads 500 --num_mini_batch 2 --episode_length 120 --num_env_steps 100000000 --ppo_epoch 15 --recurrent_policy --use_popart --lr 5e-4 --use_accumulate_grad \
-# --buffer_length 2000 --epsilon 0.4 --delta 0.4 --h 1 --archive_initial_length 500 --B_exp 150 --use_wandb
+CUDA_VISIBLE_DEVICES=0 python train_mpe_woEP.py --env_name ${env} --algorithm_name ${algo} --scenario_name ${scenario} --num_agents ${num_agents} --num_landmarks ${num_landmarks} --seed ${seed} --n_rollout_threads 500 --num_mini_batch 2 --episode_length 120 --num_env_steps 100000000 --ppo_epoch 15 --recurrent_policy --use_popart --lr 5e-4 --use_accumulate_grad \
+--buffer_length 2000 --epsilon 0.4 --delta 0.4 --h 1 --archive_initial_length 500 --B_exp 150 
+# --use_wandb
 
 
